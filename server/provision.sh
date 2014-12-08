e765691d4152fb1356f975a95b6a14216f0f65fd
Ryan Mahoney <ryan@virtuecenter.com>
2014-12-08 17:35:37 -0500

    Update provision.sh

--- server/provision.sh
+++ server/provision.sh
@@ -8,7 +8,7 @@ sudo echo "deb http://packages.elasticsearch.org/elasticsearch/1.3/debian stable
 sudo apt-get update 2> /dev/null
 
 # install software
-sudo apt-get install -y wget curl make autoconf vim nano mlocate nginx openssl default-jre mongodb-server elasticsearch git memcached libmemcached-tools beanstalkd php5 php5-cli php5-fpm php-pear php5-dev php5-gd php5-curl php5-mcrypt php5-curl php5-tidy php5-mongo php5-memcache 2> /dev/null
+sudo apt-get install -y wget curl make autoconf vim nano mlocate nginx openssl default-jre mongodb-server elasticsearch git memcached libmemcached-tools beanstalkd php5 php5-cli php5-fpm php-pear php5-dev php5-gd php5-curl php5-mcrypt php5-mongo php5-memcache 2> /dev/null
 
 # php configuration
 sudo pecl install yaml
@@ -27,20 +27,20 @@ mkdir /var/www/imagecache
 chown vagrant /var/www --recursive
 chgrp vagrant /var/www --recursive
 sudo ln -s /var/www/project/server/local.vhost.conf /etc/nginx/sites-enabled/vhost.conf
-sudo /etc/init.d/nginx stop
-sudo /etc/init.d/nginx start
-sudo /etc/init.d/elasticsearch stop
-sudo /etc/init.d/elasticsearch start
-sudo /etc/init.d/beanstalkd stop
-sudo /etc/init.d/beanstalkd start
-sudo service mongodb restart
-sudo /etc/init.d/nginx stop
-sudo killall php5-fpm
-sudo /etc/init.d/php5-fpm start
+sudo /etc/init.d/nginx stop || true
+sudo /etc/init.d/nginx start || true
+sudo /etc/init.d/elasticsearch stop || true
+sudo /etc/init.d/elasticsearch start || true
+sudo /etc/init.d/beanstalkd stop || true
+sudo /etc/init.d/beanstalkd start || true
+sudo service mongodb restart || true
+sudo /etc/init.d/nginx stop || true
+sudo killall php5-fpm || true
+sudo /etc/init.d/php5-fpm start || true
 
 # seed project
 cd /var/www/project && curl -L https://github.com/Opine-Org/Boilerplate/tarball/master | tar zx --strip-components=1 && composer install
 sudo mongo admin < /home/webuser/project/vendor/opine/build/static/database.js
 OPINE_ENV=default && export OPINE_ENV && cd /var/www/project && ./bin/opine build
 
-exit 0;
+exit 0
